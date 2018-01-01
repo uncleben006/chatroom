7331a116a8b3ca8c48b7aea423120ea181434c77
王柏元 <uncleben006@gmail.com>
2018-01-01 20:22:28 +0800

    set up the chatroom

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Navbar></Navbar>  
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from './components/partials/Navbar'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Navbar
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+$border-width: 2px;
+$card-border-width: 2px;
+@import "~bootstrap/scss/bootstrap";
+
+.router-link-active {
+  color: $brand-primary !important;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Hello from '@/components/Hello'
+import Static from '@/components/Static'
+import Chatroom from '@/components/Chatroom'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/hello',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/static',
+      name: 'Static',
+      component: Static
+    },
+    {
+      path: '/chatroom',
+      name: 'Chatroom',
+      component: Chatroom
     }
   ]
 })
