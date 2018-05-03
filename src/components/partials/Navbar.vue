da7dd5341670bfc58cfad1fd651d2ca2b5773813
王柏元 <uncleben006@gmail.com>
2018-05-03 14:49:28 +0800

    update

--- src/components/partials/Navbar.vue
+++ src/components/partials/Navbar.vue
@@ -4,30 +4,18 @@
       <button class="navbar-toggler" type="button" @click=" navValue = navValue ? false : true ">
         <span class="navbar-toggler-icon"></span>
       </button>
-    </a>
-    <!-- <a class="navbar-brand" href="/index/">Navbar
-      <button class="navbar-toggler" type="button" @click=" navValue = navValue ? false : true ">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-    </a>    -->
-  
+    </a>  
     <div class="navbar-collapse" :class="{ open : navValue }" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item" @click="navValue = false">
           <router-link class="nav-link" :to="{ path: '/index' }" >入口頁面</router-link>
         </li>
-        <!-- <li class="nav-item">
-          <router-link class="nav-link" :to="{ path: '/static' }">樣板頁面</router-link>
-        </li> -->
-        <!-- <li class="nav-item">
-          <router-link class="nav-link" :to="{ path: '/chatroom' }">disconnect聊天室</router-link>
-        </li> -->
         <li class="nav-item" @click="navValue = false">
           <router-link class="nav-link" :to="{ path: '/firechatroom' }" >聊天室</router-link>
         </li>
-        <!-- <li class="nav-item">
+        <li class="nav-item">
           <router-link class="nav-link" :to="{ path: '/board-game' }" @click="navValue = false">桌遊店家</router-link>
-        </li> -->
+        </li>
       </ul>
     </div>
   </nav>  
@@ -51,14 +39,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* * {
-  transition: 1s
-} */
 .navbar-toggler{
   float: right;
-}
-.navbar-collapse.open ul {
-  /* margin-top: 0px; */
 }
 .navbar-collapse.open ul li {
   margin-top: 0px;
@@ -66,7 +48,6 @@ export default {
 }
 @media (max-width: 992px) {
   .navbar-collapse ul {
-    /* margin-top: 0px; */
     overflow: hidden; 
     -moz-transition: margin-top 1s;
     -o-transition: margin-top 1s;
@@ -98,11 +79,4 @@ export default {
     height: 50px;
   }
 }
-/* div.dropdown.closed ul {
-  margin-top: 0px;
-}
-div.dropdown.closed ul li {
-  margin-top: -40px;
-  height: 0px;
-} */
 </style>
